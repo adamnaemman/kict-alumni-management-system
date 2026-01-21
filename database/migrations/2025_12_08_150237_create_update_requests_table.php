e6dadbac43dbb17c90fe0322dfd8280cf3ee8326
adamnaeman <159782996+adamnaemman@users.noreply.github.com>
2026-01-21 23:28:34 +0800

    Delete unused migration for update_requests_table
    Remove obsolete migration for update requests table.

--- database/migrations/2025_12_08_150237_create_update_requests_table.php
+++ database/migrations/2025_12_08_150237_create_update_requests_table.php
@@ -1,5 +1,5 @@
 <?php
-
+// This migrations not use anymore, becasue it is implemented for the previous version only, after the risk management has been check by madam in the class.
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
